166becbda042eadbd8cdc49064bc25ca002fd9f2
Valeria Meza <valeriameza173@gmail.com>
2020-06-22 16:33:00 -0500

    completed the first part of the exercise.

--- src/main/java/com/codeup/blog/controllers/DiceController.java
+++ src/main/java/com/codeup/blog/controllers/DiceController.java
@@ -5,17 +5,25 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.Random;
+
 @Controller
 public class DiceController {
 
     @GetMapping("/roll-dice")
     public String dice() {
+
         return "roll-dice";
     }
 
     @GetMapping("/roll-dice/{n}")
     public String diceNumber(@PathVariable int n, Model model) {
+        int min = 1;
+        int max = 6;
+        int random = (int) (Math.random() * (max - min + 1)) + min;
         model.addAttribute("n", n);
+        model.addAttribute("random", random);
+        model.addAttribute("guessed", random == n);
         return "roll-dice";
     }
 }
