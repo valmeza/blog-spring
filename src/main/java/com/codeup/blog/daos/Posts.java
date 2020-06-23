a0fcd678d434f68169af8f2668e9a89613a4eb8e
Valeria Meza <valeriameza173@gmail.com>
2020-06-23 14:31:34 -0500

    completed the first five steps of the exercise

---
+++ src/main/java/com/codeup/blog/daos/Posts.java
@@ -0,0 +1,8 @@
+package com.codeup.blog.daos;
+
+import com.codeup.blog.models.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Posts extends JpaRepository<Post, Long> {
+
+}
--- src/main/java/com/codeup/blog/models/Post.java
+++ src/main/java/com/codeup/blog/models/Post.java
@@ -1,10 +1,32 @@
 package com.codeup.blog.models;
+import javax.persistence.*;
 
+@Entity
+@Table(name="posts")
 public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, length = 200)
     private String title;
+
+    @Column(nullable = false)
     private String body;
 
+    // spring framework uses this empty constructor
+    public Post() {}
+
+    //insert
     public Post(String title, String body) {
+        this.title = title;
+        this.body = body;
+    }
+
+    // read
+    public Post(long id, String title, String body) {
+        this.id = id;
         this.title = title;
         this.body = body;
     }
@@ -23,5 +45,13 @@ public class Post {
 
     public void setBody(String body) {
         this.body = body;
+    }
+
+    public long getId() {
+        return this.id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
     }
 }
