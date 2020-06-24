2880d1aa91a16a348fe997463481dd9ea2ba2eef
Valeria Meza <valeriameza173@gmail.com>
2020-06-24 11:50:55 -0500

    added a form to edit and delete posts.

--- src/main/java/com/codeup/blog/controllers/PostController.java
+++ src/main/java/com/codeup/blog/controllers/PostController.java
@@ -1,12 +1,10 @@
 package com.codeup.blog.controllers;
 
+import com.codeup.blog.daos.PostsRepository;
 import com.codeup.blog.models.Post;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,18 +12,21 @@ import java.util.List;
 @Controller
 public class PostController {
 
+    // dependency injection
+    private PostsRepository postsDao;
+    public PostController(PostsRepository postsRepository) {
+        postsDao = postsRepository;
+    }
+
     @GetMapping("/post")
-    public String post(Model model) {
-        List<Post> posts = new ArrayList<>();
-        posts.add(new Post("Post 1", "Description 1"));
-        posts.add(new Post("Post 2", "Description 2"));
-        posts.add(new Post("Post 3", "Description 3"));
+    public String index(Model model) {
+        List<Post> posts = postsDao.findAll();
         model.addAttribute("posts", posts);
         return "posts/index";
     }
 
     @GetMapping("/post/show/{id}")
-    public String postId(@PathVariable long id, Model model) {
+    public String show(@PathVariable long id, Model model) {
         Post post = new Post("Post", "Description");
         model.addAttribute("p", post);
         return "posts/show";
@@ -33,13 +34,46 @@ public class PostController {
 
     @GetMapping("/post/create")
     @ResponseBody
-    public String postCreate() {
+    public String showForm() {
         return "view the form for creating a post";
     }
 
     @PostMapping("/post/create")
     @ResponseBody
-    public String postPosted() {
-        return "create a new post";
+    public String save() {
+        Post newPost = new Post("My first Blog", "My first Blog post!!");
+        postsDao.save(newPost);
+        return "Create a new post";
+    }
+
+    // this finds one and displays it in the form we want to edit
+    @GetMapping("posts/{id}/edit")
+    public String showEditForm(Model model, @PathVariable long id) {
+        // find an post
+        Post post = postsDao.getOne(id);
+        model.addAttribute("post", post);
+        return "posts/edit";
+    }
+
+    @PostMapping("posts/{id}/edit")
+    @ResponseBody
+    public String update(@PathVariable long id,
+                         @RequestParam(name="title") String title,
+                         @RequestParam(name="description") String description) {
+        // find a post
+        Post found = postsDao.getOne(id); // select * from ads where id = ?
+        // edit the add
+        found.setTitle(title);
+        found.setBody(description);
+        // save the changes
+        postsDao.save(found);
+        return "Post Updated!";
+    }
+
+    @PostMapping("/posts/{id}/delete")
+    @ResponseBody
+    public String destroy(@PathVariable long id) {
+        postsDao.deleteById(id);
+        return "Post Deleted!";
     }
 }
--- src/main/java/com/codeup/blog/daos/PostsRepository.java
+++ src/main/java/com/codeup/blog/daos/PostsRepository.java
@@ -3,6 +3,6 @@ package com.codeup.blog.daos;
 import com.codeup.blog.models.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface Posts extends JpaRepository<Post, Long> {
+public interface PostsRepository extends JpaRepository<Post, Long> {
 
 }
