b003df2253efada66df51ccdf03fbd7ee2ee101a
Valeria Meza <valeriameza173@gmail.com>
2020-07-01 19:23:19 -0500

    working on bootstrap styling

--- src/main/java/com/codeup/blog/controllers/HomeController.java
+++ src/main/java/com/codeup/blog/controllers/HomeController.java
@@ -2,14 +2,12 @@ package com.codeup.blog.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    @ResponseBody
     public String home() {
-        return "This is the landing page!";
+        return "posts/home";
     }
 }
